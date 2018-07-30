f52b33335fca1281e5782fa3de5793b602806fba
ShinLin_Yoga2 <24879504+ShinLinInvestments@users.noreply.github.com>
2018-07-30 13:18:31 -0400

    Universe need lookback
    Some dates around holiday might have fewer stock returns available

--- universe.R
+++ universe.R
@@ -1,10 +1,10 @@
 library(data.table)
 
-gen.universe <- function(market.data, company.data, startDate = "2004-01-01", endDate = "2018-01-01"){
+gen.universe <- function(market.data, company.data, lookback = 5, startDate = "2004-01-01", endDate = "2018-01-01"){
 	all.dates = all.trading.dates[which((all.trading.dates >= startDate) & (all.trading.dates <= endDate))]
 	industry.secs = company.data[, unique(Ticker)]
 	lapply(as.character(all.dates), function(d){
-		mkt.secs = market.data[Date == d, unique(Ticker)]
+		mkt.secs = unique(unlist(lapply(0:(lookback-1), function(lb) market.data[Date == utils.add.bday(d, -lb), unique(Ticker)])))
 		secs.missing.inudstry = setdiff(mkt.secs, industry.secs)
 		flog.info(paste("gen universe on", d, "with", length(mkt.secs), "in mktData and", length(industry.secs), "from industry classification"))
 		if(length(secs.missing.inudstry) > 0) flog.warn(paste("Missing industry classification:", paste(secs.missing.inudstry), "in universe"))
